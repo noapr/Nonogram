be3a6a54a1e005fb408db0299af9bd1773416fe9
noapr <pariente@campus.technion.ac.il>
2023-08-26 19:10:05 +0300

    Created unittest for exceptions in puzzle class and did __str__ function

--- puzzle.py
+++ puzzle.py
@@ -10,7 +10,7 @@ class Mark(Enum):
 
 class Puzzle:
     def __init__(self, num_rows, num_columns):
-        if num_rows < 0 or num_columns < 0:
+        if num_rows <= 0 or num_columns <= 0:
             raise IndexError
 
         self.num_rows = num_rows
@@ -18,7 +18,7 @@ class Puzzle:
         self.__board = [[Mark.WHITE for x in range(num_columns)] for x in range(num_rows)]
 
     def mark(self, row, column):
-        if row > self.num_rows or column > self.num_columns or self.num_rows < 0 or self.num_columns < 0:
+        if row > self.num_rows or column > self.num_columns or row <= 0 or column <= 0:
             raise IndexError
 
         if self.__board[row][column] is Mark.WHITE:
@@ -26,11 +26,10 @@ class Puzzle:
         else:
             raise AlreadyMarkedError(f"The square in row {row} and column {column} is already marked")
 
-    def print(self):
+    def __str__(self):
+        ret = ''
         for row in self.__board:
             for s in row:
-                if s is Mark.WHITE:
-                    print(" ", end='')
-                elif s is Mark.BLACK:
-                    print("*", end='')
-        print('\n')
+                ret += str(s.value)
+            ret += '\n'
+        return ret
---
+++ tests/test_puzzle.py
@@ -0,0 +1,27 @@
+import unittest
+
+from exception import AlreadyMarkedError
+from puzzle import Puzzle, Mark
+
+
+class TestPuzzle(unittest.TestCase):
+    def test_create_illegal_puzzle(self):
+        with self.assertRaises(IndexError):
+            new_puzzle = Puzzle(0, 1)
+        with self.assertRaises(IndexError):
+            new_puzzle = Puzzle(5, -1)
+
+    def test_mark_illegal_index(self):
+        new_puzzle = Puzzle(4, 4)
+        with self.assertRaises(IndexError):
+            new_puzzle.mark(5, 4)
+        with self.assertRaises(IndexError):
+            new_puzzle.mark(-4, 1)
+        with self.assertRaises(IndexError):
+            new_puzzle.mark(2, -1)
+
+    def test_already_marked_error(self):
+        new_puzzle = Puzzle(4, 4)
+        new_puzzle.mark(3,3)
+        with self.assertRaises(AlreadyMarkedError):
+            new_puzzle.mark(3, 3)
